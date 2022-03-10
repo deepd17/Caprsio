6f362d0de7c5f2fa714a42e9c444f06fd03181b1
deepd17 <101269514+deepd17@users.noreply.github.com>
2022-03-10 19:35:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    screens:{
+        'sm': '320px',
+        '2sm': '380px',
+        '3sm': '480px',
+        'md': '768px',
+        '2md': '900px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px'
+    },
+  },
+  plugins: [],
+}
